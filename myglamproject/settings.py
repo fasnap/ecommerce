dfa6f2b3690b3fb6e437768d8fbc8174b84961c8
fasnap <fasnapkdy@gmail.com>
2021-11-01 12:46:03 +0530

    added allowed host name

--- myglamproject/settings.py
+++ myglamproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.117.179.11']
 
 
 # Application definition
