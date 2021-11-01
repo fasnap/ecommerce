130bd32ad0ba441183084a4b4eeb46525763e249
fasnap <fasnapkdy@gmail.com>
2021-11-01 12:01:29 +0530

    session timeout removed

--- myglamproject/settings.py
+++ myglamproject/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_session_timeout.middleware.SessionTimeoutMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'myglamproject.urls'
